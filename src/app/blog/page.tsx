d2068e3b1fdf4c2935c679fc1839b60228b76680
Jay Liu <jliu2589@gmail.com>
2023-09-23 13:49:17 -0400

    Map name and image

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
 import { Metadata } from 'next';
+import { useEffect } from 'react';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'Blog',
 };
 
-function page() {
+type Character = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+async function getData() {
+  const res = await fetch('https://rickandmortyapi.com/api/character');
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+async function page() {
+  const data = await getData();
+
   return (
     <div>
       <h1>Blog</h1>
+      {data?.results.map((character: Character) => (
+        <div key={character.id}>
+          <p>{character.name}</p>
+          <Image
+            src={character.image}
+            alt={character.name}
+            width={500}
+            height={500}
+          />
+        </div>
+      ))}
     </div>
   );
 }
